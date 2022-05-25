b082ca5d3892a34456c7811158d62d9cde1ce7d9
abhm1999 <101570365+abhm1999@users.noreply.github.com>
2022-05-25 21:42:23 +0530

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='container'>
+        <div>
+            <h3>LOGOBAKERY</h3>
+        </div>
+        <div id='inside'>
+            <h6>Services</h6>
+            <h6>Projects</h6>
+            <h6>About</h6>
+        </div>
+        <div id='btn'>
+            <button>Contact</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
